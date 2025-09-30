ba034ef4b4c4fdd947c9f9d960011a77add8fc4f
Shatakshi Shukla <shatakshishukla49@gmail.com>
2025-09-30 22:25:39 +0530

    Connecting Mongoose with JS

---
+++ index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/MovieApp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.log("Error connecting to MongoDB:", error);
+    });
